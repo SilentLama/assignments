db7cc2d442046041dc570f339876890938f6ae36
SilentLama <61166264+SilentLama@users.noreply.github.com>
2021-10-27 17:54:49 +0200

    Added more docstrings

--- assignment_2/src/exercises/Student.java
+++ assignment_2/src/exercises/Student.java
@@ -23,42 +23,77 @@ public class Student {
     }
 
     public String getFirstName() {
+        /**
+         * @return the first name of the student
+         */
         return this.firstName;
     }
 
     public void setFirstName(String firstName) {
+        /**
+         * Sets The first name of the student to the provided first name
+         * @param firstName the new first name of the student
+         */
         this.firstName = firstName;
     }
 
     public String getLastName() {
+        /**
+         * @return the last name of the student
+         */
         return this.lastName;
     }
 
     public void setLastName(String lastName) {
+        /**
+         * Sets the last name of the student to the provided last name
+         * @param lastName the new last name of the student
+         */
         this.lastName = lastName;
     }
 
     public long getStudentId() {
+        /**
+         * @return the student ID of the student
+         */
         return this.studentId;
     }
 
     public double getWeight() {
+        /**
+         * @return the weight of the student
+         */
         return this.weight;
     }
 
     public void setWeight(double weight) {
+        /**
+         * Sets the weight of the student to the provided weight. Ideally this should be a positive double....
+         * @param weight the new weight of the student.
+         */
         this.weight = weight;
     }
 
     public Date getBirthday() {
+        /**
+         * @return the birthday of the student
+         */
         return this.birthday;
     }
 
     public void setBirthday(Date birthday) {
+        /**
+         * Sets the birthday of the student to the provided birthday
+         * @param birthday the new birthday of the student
+         */
         this.birthday = birthday;
     }
 
     public String getName() {
+        /**
+         * Returns the full name of the student.
+         * @return the concatenated name of the student, separated by whitespace
+         */
         return this.firstName + " " + this.lastName;
     }
 
