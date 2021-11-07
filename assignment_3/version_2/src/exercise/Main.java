78e7fe89be17bb104435c8e4e0023885f3b8ff64
SilentLama <61166264+SilentLama@users.noreply.github.com>
2021-11-07 12:56:25 +0100

    Added version 2

---
+++ assignment_3/version_2/src/exercise/Main.java
@@ -0,0 +1,48 @@
+package exercise;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int max = 1000;
+        int factor = 623;
+        int increment = 525;
+        int start = 157;
+        boolean active = true;
+        int actualrandom;
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.print("Random Seed? (y/n): ");
+        String input;
+        try {
+            input = reader.readLine();
+        } catch (Exception e) {
+            input = "n";
+        }
+        if (input.compareTo("y") == 0) {
+            // get the current date as number
+            // use the formula from slide 23
+            // cast back into integer
+            long current_date =  new Date().getTime();
+            actualrandom = (int) (current_date / 10) % max;
+        } else {
+            actualrandom = ((factor * start) + increment) % max;
+        }
+
+
+        do {
+            System.out.print(actualrandom + "\n");
+            actualrandom = ((factor * actualrandom) + increment) % max;
+            System.out.print("Another number? (y/n): ");
+            try {
+                input = reader.readLine();
+            } catch (Exception e) {
+                input = "n";
+            }
+            active = input.compareTo("y") == 0;
+        } while(active);
+    }
+}
